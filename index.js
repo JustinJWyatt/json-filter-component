f5c555146eb33b9b4125c044b4d44c5bc5789ad8
Justin Wyatt <36140141+JustinJWyatt@users.noreply.github.com>
2018-12-01 23:17:44 -0600

    Updated into Web Component

--- index.js
+++ index.js
@@ -1,49 +1,51 @@
-class JSONFilter
+class JSONFilter extends HTMLElement
 {
-    constructor(options)
+    constructor()
     {
+        super();
+
         var keys = [], data = [];
+
+        var element = this;
+
+        var url = element.getAttribute('url');
+
+        var onUpdateTarget = element.getAttribute('onUpdateTarget');
 
         //{ key: "ID", hidden: true, filter: "" }
         //{ key: "Name", hidden: false, filter: "Justin" }
         //{ key: "Organization", hidden: false, filter: "HR" }
 
-        if (!options.data || (options.data.ajax && options.data.json))
+        if (!url || !onUpdateTarget)
         {
             throw '';
         }
 
-        if (options.data.ajax)
+        var request = new XMLHttpRequest();
+
+        request.onload = function ()
         {
-            var request = new XMLHttpRequest();
-             request.onload = function(){
-                if(this.readyState === 4){
-    
-                    if(this.status === 200){
-                       var res = JSON.parse(this.responseText);
-                        if (!Array.isArray(res))
-                        {
-                            throw '';
-                        }
-                         data = res;
-                         filter(data);
-                     }else{
+            if (this.readyState === 4)
+            {
+                if (this.status === 200)
+                {
+                    var res = JSON.parse(this.responseText);
+                    if (!Array.isArray(res))
+                    {
                         throw '';
                     }
+                    data = res;
+                    filter(data);
+                } else
+                {
+                    throw '';
                 }
             }
-            
-            request.open('GET', options.data.ajax.url, true);
-            
-            request.send();
         }
 
-        if (options.data.json)
-        {
-            data = options.data.json;
+        request.open('GET', url, true);
 
-            filter(data);
-        }
+        request.send();
 
         //This function is called everytime an update is triggered, like if they click a checkbox or type into the textbox
         function publishModel(data)
@@ -52,26 +54,32 @@ class JSONFilter
 
             var hidden = keys.filter(x => x.hidden);
 
-            //Deletes from the model all of the properties
-            hidden.forEach((hiddenValue, hiddenIndex, hiddenArray) =>
+            if (hidden)
             {
-                model.map((modelValue, modelIndex, modelArray) =>
+                //Deletes from the model all of the properties
+                hidden.forEach((hiddenValue, hiddenIndex, hiddenArray) =>
                 {
-                    delete modelValue[hiddenValue.key];
+                    model.map((modelValue, modelIndex, modelArray) =>
+                    {
+                        delete modelValue[hiddenValue.key];
+                    });
                 });
-            });
+            }
 
             var filter = keys.filter(x => x.filter);
 
             var temp = [];
 
-            //If I have multiple filters, I can concat them into the model and return it.
-            filter.forEach((filterValue, filterIndex, filterArray) =>
+            if (filter)
             {
-                var result = model.filter(x => x[filterValue.key].indexOf(filterValue.filter) !== -1);
+                //If I have multiple filters, I can concat them into the model and return it.
+                filter.forEach((filterValue, filterIndex, filterArray) =>
+                {
+                    var result = model.filter(x => x[filterValue.key].toLowerCase().indexOf(filterValue.filter.toLowerCase()) !== -1);
 
-                temp = temp.concat(result);
-            });
+                    temp = temp.concat(result);
+                });
+            }
 
             return model.concat(temp);
         }
@@ -80,25 +88,25 @@ class JSONFilter
         {
             var template = document.createElement('template');
 
-            template.innerHTML = `<div class="table-legend">
-                                    <h4 class="table-legend-header">Legend</h4>
-                                    <div class="table-data-container"></div>
-                                  </div>`;
+            template.innerHTML = `<div class="legend">
+                                    <h4 class="legend-header">Table Title</h4>
+                                    <div class="legend-label-container">
+                                    </div>
+                                 </div>`;
+
+            element.appendChild(template.content.querySelector('.legend-label-container').cloneNode(true));
 
             for (var key in Object.keys(data[0]))
             {
-                keys.push({
-                    key: key,
-                    hidden: false,
-                    filter: ''
-                });
+                keys.push({ key: key, hidden: false, filter: '' });
 
-                var label = new HTMLLabelElement();
+                var label = document.createElement('label');
+                label.classList.add('legend-label');
 
-                var labelCheckBox = new HTMLInputElement();
+                var labelCheckBox = document.createElement('input');
                 labelCheckBox.type = 'checkbox';
                 labelCheckBox.setAttribute('checked', 'checked');
-                label.setAttribute('value', key);
+                labelCheckBox.setAttribute('name', key);
 
                 labelCheckBox.addEventListener('click', function (e)
                 {
@@ -113,7 +121,7 @@ class JSONFilter
 
                         keys[keyIndex].filter = '';
 
-                        options.onUpdateTarget(publishModel(data));
+                        Function(onUpdateTarget + '(' + JSON.stringify(publishModel(data)) + ')')();
                     }
                     else
                     {
@@ -122,11 +130,11 @@ class JSONFilter
 
                         keys[keyIndex].hidden = false;
 
-                        options.onUpdateTarget(publishModel(data));
+                        Function(onUpdateTarget + '(' + JSON.stringify(publishModel(data)) + ')')();
                     }
                 });
 
-                var labelTextBox = new HTMLInputElement();
+                var labelTextBox = document.createElement('input');
                 labelTextBox.type = 'text';
 
                 labelTextBox.addEventListener('keyup', function (e)
@@ -139,7 +147,7 @@ class JSONFilter
 
                     window.setTimeout(function ()
                     {
-                        options.onUpdateTarget(model);
+                        Function(onUpdateTarget + '(' + JSON.stringify(publishModel(data)) + ')')();
 
                     }, 1000);
                 });
@@ -147,3 +155,5 @@ class JSONFilter
         }
     }
 }
+
+customElements.define('json-filter', JSONFilter);
